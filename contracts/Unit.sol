137e7153b0607d5fbea8a5cc6524222a776f59b7
ValentineCodes <Valentineorga@gmail.com>
2023-03-21 15:37:01 +0100

    Buy listed NFT
        - Added function to buy listed nfts
        - This transfers NFT to buyer and records earnings and fees for seller and Unit respectively

--- contracts/Unit.sol
+++ contracts/Unit.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 error Unit__ItemListed(address nft, uint256 tokenId);
 error Unit__ItemNotListed(address nft, uint256 tokenId);
 error Unit__NotOwner();
 error Unit__ZeroAddress();
 error Unit__NotApprovedToSpendNFT();
-error Unit__AmountMustBeGreaterThanZero();
-error Unit__BidAlreadyExists(address nft, uint256 tokenId, address bidder);
-error Unit__BidDoesNotExist(address nft, uint256 tokenId, address bidder);
+error Unit__InsufficientAmount();
+error Unit__OfferAlreadyExists(address nft, uint256 tokenId, address offer_owner);
+error Unit__OfferDoesNotExist(address nft, uint256 tokenId, address offer_owner);
 error Unit__DeadlineMustBeADayOrMore();
 error Unit__DeadlineNotReached(uint256 currentTime, uint256 deadline);
 error Unit__ZeroEarnings();
@@ -18,14 +22,14 @@ error Unit__EthTransferFailed(address to, uint256 amount);
 error Unit__NotListingOwner(address nft, uint256 tokenId);
 error Unit__InsufficientFees(uint256 feeBalance, uint256 requestedAmount);
 
-contract Unit {
+contract Unit is Ownable {
 
     struct PaymentOption {
         address token;
         uint256 price;
     }
 
-    struct Bid {
+    struct Offer {
         uint256 amount;
         uint256 deadline;
     }
@@ -71,39 +75,39 @@ contract Unit {
         uint256 price
     );
 
-    event ItemBid (
-        address indexed bidder,
+    event ItemOffer (
+        address indexed offer_owner,
         address indexed nft,
         uint256 indexed tokenId,
         uint256 amount,
         uint256 deadline
     );
 
-    event BidAmountUpdated (
-        address indexed bidder,
+    event OfferAmountUpdated (
+        address indexed offer_owner,
         address indexed nft,
         uint256 indexed tokenId,
         uint256 oldAmount,
         uint256 newAmount
     );
 
-    event BidDeadlineUpdated (
-        address indexed bidder,
+    event OfferDeadlineUpdated (
+        address indexed offer_owner,
         address indexed nft,
         uint256 indexed tokenId,
         uint256 oldDeadline,
         uint256 newDeadline
     );
 
-    event BidAccepted (
-        address indexed bidder,
+    event OfferAccepted (
+        address indexed offer_owner,
         address indexed nft,
         uint256 indexed tokenId,
         uint256 price
     );
 
-    event BidWithdrawn (
-        address indexed bidder,
+    event OfferWithdrawn (
+        address indexed offer_owner,
         address indexed nft,
         uint256 indexed tokenId
     );
@@ -129,7 +133,7 @@ contract Unit {
     uint256 private s_fees;
 
     mapping(address => uint256 => Listing) private s_listings; // nft => token id => Listing
-    mapping(address => address => uint256 => Bid) private s_bids; // bidder => nft => token id => Bid
+    mapping(address => address => uint256 => Offer) private s_offers; // offer_owner => nft => token id => Offer
     mapping(address => uint256) private s_earnings; // owner > amount
 
     modifier isListed(address nft, uint256 tokenId) {
@@ -150,14 +154,27 @@ contract Unit {
         _;
     }
 
-    function _extractFee(uint256 amount) private pure returns(uint256 earnings, uint256 fee) {
-        earnings = (amount * 997) / 1000;
-        fee = (amount * 3) / 10;
+    function getListing(address nft, uint256 tokenId) external view returns(Listing memory) {
+        return s_listings[nft][tokenId];
     }
 
+    function getEarnings(address seller) external view returns(uint256) {
+        return s_earnings[seller];
+    }
+
+    function getFees() external view returns(uint256) {
+        return s_fees;
+    }
+
+    function getOffer(address offer_owner, address nft, uint256 tokenId) external view returns(Offer memory offer) {
+        offer = s_offers[offer_owner][nft][tokenId];
+        if(offer.amount <= 0) revert Unit__OfferDoesNotExist(nft, tokenId, offer_owner);
+    }
+
+    // TO-DO: Approve Unit to spend NFT
     function listItem(address nft, uint256 tokenId, uint256 price) external isNotListed(nft, tokenId) isOwner(nft, tokenId, msg.sender) {
         if(nft == address(0)) revert Unit__ZeroAddress();
-        if(price == 0) revert Unit__AmountMustBeGreaterThanZero();
+        if(price == 0) revert Unit__InsufficientAmount();
         
         IERC721 _nft = IERC721(nft);
 
@@ -169,7 +186,7 @@ contract Unit {
         emit ItemListed(msg.sender, nft, tokenId, price);     
     }
 
-    function updateSeller(address nft, uint256 tokenId, address newSeller) external isOwner(nft, tokenId, newSeller) {
+    function updateItemSeller(address nft, uint256 tokenId, address newSeller) external isOwner(nft, tokenId, newSeller) {
         Listing memory listing = s_listings[nft][tokenId];
         address oldSeller = listing.seller;
 
@@ -186,11 +203,28 @@ contract Unit {
         address oldPrice = listing.price;
 
         if(oldPrice <= 0) revert Unit__ItemNotListed(nft, tokenId);
-        if(newPrice <= 0) revert Unit__AmountMustBeGreaterThanZero();
+        if(newPrice <= 0) revert Unit__InsufficientAmount();
 
         s_listings[nft][tokenId].price = newPrice;
 
         emit ItemPriceUpdated(nft, tokenId, oldPrice, newPrice);
+    }
+
+    function buyItem(address nft, uint256 tokenId) external payable {
+        Listing memory listing = s_listings[nft][tokenId];
+    
+        if(listing.seller == address(0)) revert Unit__ItemNotListed(nft, tokenId);
+        if(listing.price < msg.value) revert Unit__InsufficientAmount();
+
+        delete s_listings[nft][tokenId];
+
+        if(IERC721(nft).safeTransferFrom(listing.seller, msg.sender, tokenId) == false) revert Unit__NftTransferFailed(msg.sender, nft, tokenId);
+    
+        (uint256 earnings, uint256 fee) = _extractFee(msg.value);
+        s_earnings[listing.seller] += earnings;
+        s_fees += fee;
+
+        emit ItemBought(msg.sender, nft, tokenId, msg.value);
     }
 
     function unlistItem(address nft, uint256 tokenId) externat isOwner(nft, tokenId, msg.sender) {
@@ -198,93 +232,93 @@ contract Unit {
         emit ItemUnlisted(msg.sender, nft, tokenId);
     }
 
-    function placeBid(address nft, uint256 tokenId, uint256 deadline) external payable {
-        if(nft == address(0)) revert Unit__ZeroAddress();
-
+    /**
+     * -- Note -- 
+     *      Use MIN_BID_DEADLINE if deadline is not specified
+     */
+    function placeOffer(address nft, uint256 tokenId, uint256 deadline) external payable {
         Listing memory listing = s_listings[nft][tokenId];
     
         if(listing.seller == address(0)) revert Unit__ItemNotListed(nft, tokenId);
-        if(s_bids[msg.sender][nft][tokenId].amount > 0) revert Unit__BidAlreadyExists(nft, tokenId, msg.sender);
-        if(msg.value <= 0) revert Unit__AmountMustBeGreaterThanZero();
-        if(deadline != 0 && deadline < block.timestamp + MIN_BID_DEADLINE) revert Unit__DeadlineMustBeADayOrMore();
-        
-        s_bids[msg.sender][nft][tokenId] = Bid({
-            amount: msg.value,
-            deadline: deadline == 0 ? MIN_BID_DEADLINE : deadline 
-        })
+        if(s_offers[msg.sender][nft][tokenId].amount > 0) revert Unit__OfferAlreadyExists(nft, tokenId, msg.sender);
+        if(msg.value <= 0) revert Unit__InsufficientAmount();
 
-        emit ItemBid(msg.sender, nft, tokenId, amount, deadline);
+        uint256 _deadline = deadline;
+
+        if(deadline == 0) {
+            _deadline = MIN_BID_DEADLINE;
+        } else if(deadline < block.timestamp + MIN_BID_DEADLINE) { 
+            revert Unit__DeadlineMustBeADayOrMore();
+        }
+
+        s_offers[msg.sender][nft][tokenId] = Offer(msg.value, _deadline);
+
+        emit ItemOffer(msg.sender, nft, tokenId, amount, deadline);
     }
 
-    function acceptBid(address nft, uint256 tokenId, address bidder) external {
-        if(nft == address(0) || bidder == address(0)) revert Unit__ZeroAddress();
-
+    function acceptOffer(address offer_owner, address nft, uint256 tokenId) external {
         Listing memory listing = s_listings[nft][tokenId];
-        Bid memory bid = s_bids[bidder][nft][tokenId];
+        Offer memory offer = s_offers[offer_owner][nft][tokenId];
         
         if(listing.seller == address(0)) revert Unit__ItemNotListed(nft, tokenId);
-        if(listing.seller != msg.sender) revert Unit__NotListingOwner(nft, tokenId);
-        if(bid.amount <= 0) revert Unit__BidDoesNotExist(nft, tokenId, bidder);
-        if(bid.deadline <= block.timestamp) revert Unit__BidExpired();
+
+        if(offer.amount <= 0) revert Unit__OfferDoesNotExist(nft, tokenId, offer_owner, offer_owner);
+        if(offer.deadline <= block.timestamp) revert Unit__OfferExpired();
 
         delete s_listings[nft][tokenId];
-        delete s_bids[bidder][nft][tokenId];
+        delete s_offers[offer_owner][nft][tokenId];
         
-        if(IERC721(nft).safeTransferFrom(listing.seller, bidder, tokenId) == false) revert Unit__NftTransferFailed(bidder, nft, tokenId);
+        if(IERC721(nft).safeTransferFrom(listing.seller, offer_owner, tokenId) == false) revert Unit__NftTransferFailed(offer_owner, nft, tokenId);
     
-        (uint256 earnings, uint256 fee) = _extractFee(bid.amount);
+        (uint256 earnings, uint256 fee) = _extractFee(offer.amount);
         s_earnings[listing.seller] += earnings;
         s_fees += fee;
 
-        emit BidAccepted(bidder, nft, tokenId, bid.amount);
+        emit OfferAccepted(offer_owner, nft, tokenId, offer.amount);
 
     }
 
-    function updateBidAmount(address nft, uint256 tokenId, uint256 newAmount) external {
-        if(nft == address(0)) revert Unit__ZeroAddress();
-
+    function updateOfferAmount(address nft, uint256 tokenId, uint256 newAmount) external {
         Listing memory listing = s_listings[nft][tokenId];
-        uint256 oldAmount = s_bids[msg.sender][nft][tokenId].amount;
+        uint256 oldAmount = s_offers[msg.sender][nft][tokenId].amount;
 
         if(listing.seller == address(0)) revert Unit__ItemNotListed(nft, tokenId);
-        if(oldAmount <= 0) revert Unit__BidDoesNotExist(nft, tokenId, msg.sender);
-        if(newAmount <= 0) revert Unit__AmountMustBeGreaterThanZero();
+        if(oldAmount <= 0) revert Unit__OfferDoesNotExist(nft, tokenId, msg.sender);
+        if(newAmount <= 0) revert Unit__InsufficientAmount();
 
-        s_bids[msg.sender][nft][tokenId].amount = newAmount;
+        s_offers[msg.sender][nft][tokenId].amount = newAmount;
 
-        emit BidAmountUpdated(msg.sender, nft, tokenId, oldAmount, newAmount);
+        emit OfferAmountUpdated(msg.sender, nft, tokenId, oldAmount, newAmount);
     }
 
-    function updateBidDeadline(address nft, uint256 tokenId, uint256 newDeadline) external {
-        if(nft == address(0)) revert Unit__ZeroAddress();
+    // function updateOfferDeadline(address nft, uint256 tokenId, uint256 newDeadline) external {
+    //     if(nft == address(0)) revert Unit__ZeroAddress();
 
-        Listing memory listing = s_listings[nft][tokenId];
-        uint256 oldDeadline = s_bids[msg.sender][nft][tokenId].deadline;
+    //     Listing memory listing = s_listings[nft][tokenId];
+    //     uint256 oldDeadline = s_offers[msg.sender][nft][tokenId].deadline;
 
-        if(listing.seller == address(0)) revert Unit__ItemNotListed(nft, tokenId);
-        if(oldDeadline <= 0) revert Unit__BidDoesNotExist(nft, tokenId, msg.sender);
-        if(newDeadline < block.timestamp + MIN_BID_DEADLINE) revert Unit__DeadlineMustBeADayOrMore();
+    //     if(listing.seller == address(0)) revert Unit__ItemNotListed(nft, tokenId);
+    //     if(oldDeadline <= 0) revert Unit__OfferDoesNotExist(nft, tokenId, msg.sender);
+    //     if(newDeadline < block.timestamp + MIN_BID_DEADLINE) revert Unit__DeadlineMustBeADayOrMore();
 
-        s_bids[msg.sender][nft][tokenId].deadline = newDeadline;
+    //     s_offers[msg.sender][nft][tokenId].deadline = newDeadline;
 
-        emit BidAmountUpdated(msg.sender, nft, tokenId, oldDeadline, newDeadline);
-    }
+    //     emit OfferAmountUpdated(msg.sender, nft, tokenId, oldDeadline, newDeadline);
+    // }
 
-    function withdrawBids(address nft, uint256 tokenId) external {
-        if(nft == address(0)) revert Unit__ZeroAddress();
+    function withdrawOffers(address nft, uint256 tokenId) external {
+        Offer memory offer = s_offers[msg.sender][nft][tokenId];
 
-        Bid memory bid = s_bids[msg.sender][nft][tokenId];
+        if(offer.amount <= 0) revert Unit__OfferDoesNotExist();
+        if(offer.deadline > block.timestamp) revert Unit__DeadlineNotReached(block.timestamp, offer.deadline);
 
-        if(bid.amount <= 0) revert Unit__BidDoesNotExist();
-        if(bid.deadline > block.timestamp) revert Unit__DeadlineNotReached(block.timestamp, bid.deadline);
+        delete s_offers[msg.sender][nft][tokenId];
 
-        delete s_bids[msg.sender][nft][tokenId];
+        (bool success, ) = payable(msg.sender).call{value: offer.amount}("");
 
-        (bool success, ) = payable(msg.sender).call{value: bid.amount}("");
+        if(!success) revert Unit__EthTransferFailed(msg.sender, offer.amount);
 
-        if(!success) revert Unit__EthTransferFailed(msg.sender, bid.amount);
-
-        emit BidWithdrawn(bidder, nft, tokenId)
+        emit OfferWithdrawn(offer_owner, nft, tokenId)
     }
 
     function withdrawEarnings() external {
@@ -315,7 +349,14 @@ contract Unit {
         emit FeesWithdrawn(msg.sender, amount);
     }
 
+    /** 
+     * @title Helper Functions
+     */
 
+    function _extractFee(uint256 amount) private pure returns(uint256 earnings, uint256 fee) {
+        earnings = (amount * 997) / 1000;
+        fee = (amount * 3) / 10;
+    }
 }
 
 /**
@@ -345,9 +386,9 @@ contract Unit {
  *      nft address
  *      token id
  *      mapping of approved PaymentOptions(Note: ETH => WETH => PaymentOption)
- *      mapping of bidders
+ *      mapping of offer_owners
  *
- * - Bid
+ * - Offer
  * #params
  *      amount
  *      deadline
@@ -368,13 +409,13 @@ contract Unit {
  *      nft address
  *      token id
  *
- * - ItemBid
+ * - ItemOffer
  * #params
- *      bidder address
+ *      offer_owner address
  *      nft address
  *      token id
  *      token address
- *      bid value
+ *      offer value
  *      deadline
  *
  * - ItemBought
@@ -400,34 +441,34 @@ contract Unit {
  *      old seller
  *      new seller
  *
- * - BidAmountUpdated
+ * - OfferAmountUpdated
  * #params
- *      bid owner
+ *      offer owner
  *      nft address
  *      token id
  *      token address
  *      old amount
  *      new amount
  *
- * - BidDeadlineUpdated
+ * - OfferDeadlineUpdated
  * #params
- *      bid owner
+ *      offer owner
  *      nft address
  *      token id
  *      token address
  *      old deadline
  *      new deadline
  *
- * - BidRemoved
+ * - OfferRemoved
  * #params
- *      bid owner
+ *      offer owner
  *      nft address
  *      token id
  *      token address
  * 
- * - BidAccepted
+ * - OfferAccepted
  * #params 
- *      bid owner 
+ *      offer owner 
  *      nft address 
  *      token id
  *      token address 
@@ -457,7 +498,7 @@ contract Unit {
  *      nft address
  *      token id
  *      array of approved PaymentOptions
- * #requirements
+ * #program_flow
  *      ensure item is owned by msg.sender
  *      ensure price is not zero
  *      ensure Unit is approved to transfer the nft
@@ -466,12 +507,12 @@ contract Unit {
  *      store listing
  *      emit event
  *
- * - updateSeller✅
+ * - updateItemSeller✅
  * #params
  *      new seller address
  *      nft address
  *      token id
- * #requirements
+ * #program_flow
  *      ensure item is listed
  *      ensure new seller address is not zero
  *      ensure new seller is current owner
@@ -482,17 +523,17 @@ contract Unit {
  * #params
  *      nft address
  *      token id
- * #requirements
+ * #program_flow
  *      ensure msg.sender is current seller
  *      remove item
  *      emit event
  * 
- * - buyItem
+ * - buyItem✅
  * #params
  *      nft address
  *      token id 
  *      amount(msg.value)
- * #requirements
+ * #program_flow
  *      ensure item is listed
  *      ensure amount is >= item price
  *      delete list record
@@ -507,7 +548,7 @@ contract Unit {
  *      token id
  *      token address
  *      amount
- * #requirements
+ * #program_flow
  *      ensure item is listed
  *      ensure Unit is approved to spend tokens
  *      ensure msg.sender has amount
@@ -523,86 +564,94 @@ contract Unit {
  * - withdrawEarnings✅
  * #params
  *      token address
- * #requirements
+ * #program_flow
  *      ensure token address is not zero
  *      ensure msg.sender has earnings(amount > 0)
  *      delete record
  *      transfer earnings to msg.sender
  *      emit event
  *
- * - placeBid✅
+ * - placeOffer✅
  * #params
  *      nft address
  *      token id
  *      token address
  *      amount
  *      deadline
- * #requirements
+ * #program_flow
  *      approve Unit to spend tokens
  *      ensure item is listed
- *      ensure bid does not exist
+ *      ensure offer does not exist
  *      ensure token address is approved for payment by item seller
  *      ensure Unit is approved to spend tokens
  *      ensure deadline is not less than MIN_BID_DEADLINE
  *      if no deadline is specified, use MIN_BID_DEADLINE
- *      store in bids mapping of item
+ *      store in offers mapping of item
  *      emit event
  *
- * - acceptBid✅
+ * - acceptOffer✅
  * #params
  *      nft address
  *      token id
- *      bid owner address
+ *      offer owner address
  *      token address
- * #requirements
+ * #program_flow
  *      ensure msg.sender is listing seller
  *      ensure item is listed
- *      ensure bid exists
+ *      ensure offer exists
  *      ensure deadline has not been reached
  *      delete listed item
- *      transfer nft to bid owner
+ *      transfer nft to offer owner
  *      transfer earnings to Unit
  *      record earnings for listing seller
  *      emit event
  *
- * - updateBidAmount✅
+ * - updateOfferAmount✅
  * #params
  *      nft address
  *      token id
  *      token address
  *      amount
- * #requirements
+ * #program_flow
  *      ensure item is listed
- *      ensure bid exists
- *      ensure msg.sender owns bid
+ *      ensure offer exists
+ *      ensure msg.sender owns offer
  *      ensure amount is not zero
  *      update amount
  *      emit event
  *
- * - updateBidDeadline✅
+ * - updateOfferDeadline✅
  * #params
  *      nft address
  *      token id
  *      token address
  *      new deadline
- * #requirements
+ * #program_flow
  *      ensure item is listed
- *      ensure bid exists
- *      ensure msg.sender owns bid
+ *      ensure offer exists
+ *      ensure msg.sender owns offer
  *      ensure new deadline is not less than MIN_BID_DEADLINE
  *      update deadline
  *      emit event
  *
- * - removeBid✅
+ * - removeOffer✅
  * #params
  *      nft address
  *      token id
  *      token address
- * #requirements
+ * #program_flow
  *      ensure item is listed
- *      ensure bid exists
- *      ensure msg.sender owns bid
- *      delete bid
+ *      ensure offer exists
+ *      ensure msg.sender owns offer
+ *      delete offer
  *      emit event
+ * 
+ * - getListing✅
+ * 
+ * - getEarnings✅
+ * 
+ * - getFees✅
+ * 
+ * - getOffer✅
  *
  */
